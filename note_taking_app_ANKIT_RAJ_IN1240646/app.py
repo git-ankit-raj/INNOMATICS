59e0e9aad7df552773a88bd9e05af49cb12a5cde
Ankit-Raj <52020415+git-ankit-raj@users.noreply.github.com>
2024-02-28 12:43:41 +0530

    Add files via upload

---
+++ note_taking_app_ANKIT_RAJ_IN1240646/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+notes = []
+
+# @app.route('/', methods=["POST"])
+@app.route('/', methods=["GET", "POST"])
+
+def index():
+    if request.method == "POST":
+        # note = request.args.get("note")
+        note = request.form.get("note")
+        notes.append(note)
+
+    return render_template("home.html", notes=notes)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
